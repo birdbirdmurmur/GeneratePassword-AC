ca28a5d648670e387e8cecdfa650a7dadff98709
BirdBird <birdbirdmurmur@gmail.com>
2023-07-24 16:09:55 +0800

    feat: add index and main templates

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) =>{
-    res.send('Hello World !')
+    res.render('index')
 })
 
 app.listen(port, () =>{
